5808924dbe86f11c81d7707897af7b5c5b109f99
Adriano Santana <adriano.silva@kognit.com.br>
2023-10-31 12:45:48 -0300

    Exclude project User.Api

--- User.Api/User.Api/Dto/OccupationDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace User.Api.Dto
-{
-    public class OccupationDto
-    {
-        public int Id { get; set; }
-        public string NameOccupation { get; set; }
-    }
-}
--- User.Api/User.Api/Dto/PhoneDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace User.Api.Dto
-{
-    public class PhoneDto
-    {
-        public int Id { get; set; }
-        public string DDD { get; set; }
-        public string Number { get; set; }
-    }
-}
